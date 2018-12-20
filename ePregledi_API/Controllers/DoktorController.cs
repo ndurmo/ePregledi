fb65d83f2f682b9e90efe2eb4e93338ab87b883d
Naid Durmo <naid.durmo@edu.fit.ba>
2018-12-20 23:25:32 +0100

    Initial commit

---
+++ ePregledi_API/Controllers/DoktorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using ePregledi_API.Models;
+
+namespace ePregledi_API.Controllers
+{
+    public class DoktorController : ApiController
+    {
+        private ePreglediEntities context = new ePreglediEntities();
+        public DoktorController()
+        {
+
+        }
+
+        // /api/doktor
+        public List<Doktor> GetDoktors()
+        {
+            return context.Doktors.Include("Osoba").ToList();
+        }
+    }
+}
