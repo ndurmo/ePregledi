128e516e8da5148b184fded67132f6e318eeffe0
ndurmo <naid.durmo@gmail.com>
2021-05-24 19:44:40 +0200

    done the whole blody thing in one piece

---
+++ ePregledi.Models/Responses/DoctorViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ePregledi.Models.Responses
+{
+    public class DoctorViewModel
+    {
+        public int DoctorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get => $"{FirstName} {LastName}"; }
+        public int NumberOfExaminations { get; set; }
+        public double AverageRating { get; set; }
+    }
+}
